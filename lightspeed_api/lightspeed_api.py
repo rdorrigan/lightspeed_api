11b9989db4ed350de5adefca18d131f9438d828b
rdorrigan <31552969+rdorrigan@users.noreply.github.com>
2023-11-30 16:12:51 -0800

    Update get
    get now iterates over get_paginated passing relevant kwargs

--- lightspeed_api/lightspeed_api.py
+++ lightspeed_api/lightspeed_api.py
@@ -9,7 +9,7 @@ __author__ = "Forrest Beck"
 
 class Lightspeed(object):
 
-	def __init__(self, config,testing=False):
+	def __init__(self, config, testing=False):
 		"""
 		Creates new Lightspeed object.
 		:param config: Specify dictionary with config
@@ -18,7 +18,8 @@ class Lightspeed(object):
 
 		self.token_url = "https://cloud.lightspeedapp.com/oauth/access_token.php"
 		if "account_id" in config:
-			self.api_url = "https://api.lightspeedapp.com/API/V3/Account/" + config["account_id"] + "/"
+			self.api_url = "https://api.lightspeedapp.com/API/V3/Account/" + \
+				config["account_id"] + "/"
 		else:
 			self.api_url = ""
 		# Initialize token as expired.
@@ -56,7 +57,8 @@ class Lightspeed(object):
 			json = r.json()
 
 			self.bearer_token = json["access_token"]
-			self.session.headers.update({'Authorization': 'Bearer ' + self.bearer_token})
+			self.session.headers.update(
+				{'Authorization': 'Bearer ' + self.bearer_token})
 
 			return json["refresh_token"]
 		except:
@@ -81,9 +83,10 @@ class Lightspeed(object):
 				r = s.post(self.token_url, data=payload)
 				json = r.json()
 				self.token_expire_time = datetime.datetime.now() + \
-										 datetime.timedelta(seconds=int(json["expires_in"]))
+					datetime.timedelta(seconds=int(json["expires_in"]))
 				self.bearer_token = json["access_token"]
-				self.session.headers.update({'Authorization': 'Bearer ' + self.bearer_token})
+				self.session.headers.update(
+					{'Authorization': 'Bearer ' + self.bearer_token})
 
 				return self.bearer_token
 			except:
@@ -100,7 +103,7 @@ class Lightspeed(object):
 		:return: results in json
 		"""
 		if self.testing:
-			print(method,url,data)
+			print(method, url, data)
 
 		if method in ("post", "put", "delete"):
 			units_needed = 10
@@ -108,25 +111,26 @@ class Lightspeed(object):
 			units_needed = 1
 
 		if self.rate_limit_availability < units_needed:
-				left_over = units_needed - self.rate_limit_availability
-				seconds_wait = left_over / self.rate_limit_bucket_rate
-				last_request = datetime.timedelta.total_seconds(datetime.datetime.now() - self.rate_limit_last_request)
-				if last_request < seconds_wait:
-					time.sleep(seconds_wait - last_request)
+			left_over = units_needed - self.rate_limit_availability
+			seconds_wait = left_over / self.rate_limit_bucket_rate
+			last_request = datetime.timedelta.total_seconds(
+				datetime.datetime.now() - self.rate_limit_last_request)
+			if last_request < seconds_wait:
+				time.sleep(seconds_wait - last_request)
 
 		try:
 			tries = 0
 			while tries <= 3:
 				# Check the bearer token is up to date.
 				self.get_token()
-				s = self.session.request(method.upper(),url,data=data)
+				s = self.session.request(method.upper(), url, data=data)
 				# Watch for too many requests status
 				if s.status_code == 429:
 					time.sleep(3)
 					tries += 1
 				else:
 					break
-			
+
 			# if s.status_code == 200:
 			try:
 				s.raise_for_status()
@@ -137,20 +141,23 @@ class Lightspeed(object):
 				requested, total = self.rate_limit_bucket_level.split('/')
 				self.rate_limit_availability = float(total) - float(requested)
 				# Update Drip Rates
-				self.rate_limit_bucket_rate = float(s.headers['X-LS-API-Drip-Rate'])
+				self.rate_limit_bucket_rate = float(
+					s.headers['X-LS-API-Drip-Rate'])
 				# return s.json()
 			except HTTPError as e:
 				print(e)
+				print(s.headers)
 			finally:
 				return s.json()
 
 		except requests.exceptions.HTTPError as e:
 			return "Error: " + str(e)
 
-	def build_url(self,source,id_=None,parameters=None,encode=False):
+	def build_url(self, source, id_=None, parameters=None, encode=False):
 		if parameters:
 			if encode:
-				url = self.api_url + source + ".json?" + parse.urlencode(parameters, safe=':-')
+				url = self.api_url + source + ".json?" + \
+					parse.urlencode(parameters, safe=':-')
 			else:
 				url = self.api_url + source + ".json?" + parameters
 		elif id_:
@@ -159,54 +166,65 @@ class Lightspeed(object):
 			url = self.api_url + source + ".json"
 		return url
 
-	def has_next(self,resp):
-		return True if resp.get('@attributes',{}).get('next',{}) else False
-	
-	def next_page(self,resp):
+	def has_next(self, resp):
+		return True if resp.get('@attributes', {}).get('next', {}) else False
+
+	def next_page(self, resp):
 		if self.has_next(resp):
 			next_page = resp['@attributes']['next']
 			return self.request_bucket("get", next_page)
 		return None
-	def has_previous(self,resp):
-		return True if resp.get('@attributes',{}).get('previous',{}) else False
-	
-	def previous_page(self,resp):
+
+	def has_previous(self, resp):
+		return True if resp.get('@attributes', {}).get('previous', {}) else False
+
+	def previous_page(self, resp):
 		if self.has_previous(resp):
 			previous_page = resp['@attributes']['previous']
 			return self.request_bucket("get", previous_page)
 		return None
 
-	def get(self, source, id_=None, parameters=None):
+	def get(self, source, id_=None, parameters=None, keep_attributes=False):
 		"""
-		Get data from API. Implement pagination.
+		Get data from API.
 		:param source: API Source desired
 		:param parameters: Optional URL Parameters.
 		:return: JSON Results
-		
+
 		"""
 
 		# Check the bearer token is up to date.
-		
-		url = self.build_url(source=source,id_=id_,parameters=parameters,encode=True)
+		data = []
+		for p in self.get_paginated(source,id_,parameters,keep_attributes):
+			if isinstance(p,list):
+				data.extend(p)
+			elif isinstance(p,dict):
+				data.append(p)
+		return data
+		# url = self.build_url(source=source, id_=id_,
+		# 					 parameters=parameters, encode=True)
 
-		r = self.request_bucket("get", url)
+		# r = self.request_bucket("get", url)
 
-		if isinstance(r,dict):
-			if r:
-				if r.get('@attributes',{}).get('next',{}):
-					next_page = r['@attributes']['next']
-					while True:
-						p = self.request_bucket("get", next_page)
-						next_page = p.get('@attributes',{}).get('next',{})
-						# Append new data to original request
-						for i in p:
-							if type(p[i]) == list:
-								r[i].extend(p[i])
+		# if isinstance(r, dict):
+		# 	if r:
+		# 		next_resp = self.next_page(r)
+		# 		while next_resp:
+		# 			# if r.get('@attributes',{}).get('next',{}):
+		# 			# next_page = r['@attributes']['next']
+		# 			# while next_page:
+		# 			# p = self.request_bucket("get", next_page)
+		# 			# next_page = p.get('@attributes',{}).get('next',{})
+		# 			# Append new data to original request
+		# 			for i in next_resp:
+		# 				if isinstance(next_resp[i], list):
+		# 					r[i].extend(next_resp[i])
+		# 			next_resp = self.next_page(next_resp)
+		# 			# if not next_page:
+		# 			# 	break
+		# return r
 
-						if not next_page:
-							break
-		return r
-	def get_paginated(self, source, id_=None, parameters=None,keep_attributes=True):
+	def get_paginated(self, source, id_=None, parameters=None, keep_attributes=True):
 		"""
 		Get data from API. Implement pagination.
 		:param source: API Source desired
@@ -217,8 +235,9 @@ class Lightspeed(object):
 		"""
 
 		# Check the bearer token is up to date.
-		
-		url = self.build_url(source=source,id_=id_,parameters=parameters,encode=True)
+
+		url = self.build_url(source=source, id_=id_,
+							 parameters=parameters, encode=True)
 
 		r = self.request_bucket("get", url)
 		if not keep_attributes:
@@ -229,7 +248,10 @@ class Lightspeed(object):
 		next_resp = self.next_page(r)
 		while next_resp:
 			if not keep_attributes:
-				yield next_resp[source]
+				try:
+					yield next_resp[source]
+				except:
+					break
 			else:
 				yield next_resp
 			next_resp = self.next_page(next_resp)
@@ -241,14 +263,12 @@ class Lightspeed(object):
 		:param data: PUT Data
 		:param parameters: Optional URL Parameters.
 		:return: JSON Results
-		
-		"""
 
-		
+		"""
 
 		d = json.dumps(data)
 
-		url = self.build_url(source=source,id_=id_,parameters=parameters)
+		url = self.build_url(source=source, id_=id_, parameters=parameters)
 
 		r = self.request_bucket("put", url, d)
 		return r
@@ -262,15 +282,14 @@ class Lightspeed(object):
 		:return: JSON Results
 		"""
 
-
 		d = json.dumps(data)
 
-		url = self.build_url(source=source,parameters=parameters)
+		url = self.build_url(source=source, parameters=parameters)
 
 		r = self.request_bucket("post", url, d)
 		return r
 
-	def delete(self, source,id_=None, parameters=None):
+	def delete(self, source, id_=None, parameters=None):
 		"""
 		Delete object from API
 		:param source: API Source
@@ -278,8 +297,7 @@ class Lightspeed(object):
 		:return: JSON Results
 		"""
 
-
-		url = self.build_url(source=source,id_=id_,parameters=parameters)
+		url = self.build_url(source=source, id_=id_, parameters=parameters)
 
 		r = self.request_bucket("delete", url)
 		return r
@@ -295,7 +313,7 @@ class Lightspeed(object):
 		"""
 
 		# Check the bearer token is up to date.
-		return self.post(source=source,data=data,parameters=parameters)
+		return self.post(source=source, data=data, parameters=parameters)
 
 	def update(self, source, data, id_=None, parameters=None):
 		"""
@@ -308,8 +326,4 @@ class Lightspeed(object):
 		"""
 
 		# Check the bearer token is up to date.
-		return self.put(source=source,data=data,id_=id_,parameters=parameters)
-
-
-	
-	
+		return self.put(source=source, data=data, id_=id_, parameters=parameters)
